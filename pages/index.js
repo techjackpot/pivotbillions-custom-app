03da191c974f7beac97272fb920a25d020e73089
Lancelot Gordon <lance032017@gmail.com>
2020-11-24 04:13:09 -0500

    initial shopify node.js + react files

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>This serves multiple shopify sites</Heading>
+  </Page>
+);
+
+export default Index;
